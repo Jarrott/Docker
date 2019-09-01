547a6984ff410f185d058c6807b32a1f3d5f14a0
Little student <xqiqio7@gmail.com>
2019-09-01 23:57:24 +0800

    Update edit_ubuntu.sh

--- edit_ubuntu.sh
+++ edit_ubuntu.sh
@@ -3,5 +3,6 @@ mv /etc/apt/sources.list /etc/apt/sources_init.list
 
 cd /etc/apt/ && wget https://github.com/Jarrott/Docker/raw/master/sources.list
 
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
 
 apt-get update -y && apt-get vim -y
