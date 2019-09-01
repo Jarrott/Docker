74b0f0f42377980aff7d6400b41715a1c401cce6
Little student <xqiqio7@gmail.com>
2019-09-02 00:39:42 +0800

    Update edit_debian.sh

--- debian/edit_debian.sh
+++ debian/edit_debian.sh
@@ -1,9 +1,3 @@
 mv /etc/apt/sources.list /etc/apt/sources_init.list
 
 cd /etc/apt/ && wget https://github.com/Jarrott/Docker/raw/master/debian/sources.list
-
-source /etc/apt/sources.list
-
-apt-get update 
-
-apt-get vim -y
